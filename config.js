02fa9a9b63a66ee1ba8c875b0372582c9c23c237
raniarhaiem <rania.rhaiem@esprit.tn>
2023-12-23 12:22:35 +0100

    final config

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     dbConfig: {
-      host: process.env.DB_HOST || '',
+      host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || 3306,
-      user: process.env.DB_USER || '',
+      user: process.env.DB_USER || 'root',
       password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || '',
+      database: process.env.DB_NAME || 'test',
     },
   };
   
